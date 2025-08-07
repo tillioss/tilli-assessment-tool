383b4dcbea73bdfcbee44e8874ac8371791197f1
anjula-sack <anjulashanaka@gmail.com>
2025-08-07 08:35:58 +0530

    Add instructions

--- src/app/instructions/page.tsx
+++ src/app/instructions/page.tsx
@@ -3,6 +3,13 @@ import Link from 'next/link'
 import React, { Suspense, useState, useEffect } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import Button from '../components/button'
+import instruction1 from '@/assets/audios/instructions/instruction_1.mp3'
+import instruction2 from '@/assets/audios/instructions/instruction_2.mp3'
+import instruction3 from '@/assets/audios/instructions/instruction_3.mp3'
+import instruction4 from '@/assets/audios/instructions/instruction_4.mp3'
+import instruction5 from '@/assets/audios/instructions/instruction_5.mp3'
+import instruction6 from '@/assets/audios/instructions/instruction_6.mp3'
+import AudioIcon from '@/assets/svg/AudioIcon'
 
 enum Step {
   PLAY,
@@ -16,6 +23,15 @@ export default function Page() {
     </Suspense>
   )
 }
+
+const instructionAudios = [
+  instruction1,
+  instruction2,
+  instruction3,
+  instruction4,
+  instruction5,
+  instruction6,
+]
 
 function PageContent() {
   const [step, setStep] = useState(Step.PLAY)
@@ -43,13 +59,33 @@ function PageContent() {
         </div>
 
         <div className="justify-center items-center flex flex-col h-[90vh] px-4">
-          <h2 className="text-lg font-semibold text-gray-700 text-center">
+          <h2 className="text-xl font-semibold text-gray-700 text-center mb-10">
             Read the instructions carefully
           </h2>
 
           <div className="bg-white p-4 md:p-6 rounded-xl text-left relative w-full max-w-md">
             <div className="absolute top-4 right-4 text-gray-500 cursor-pointer hover:text-black">
-              {/* <Volume2 size={24} /> */}
+              <button
+                onClick={() => {
+                  let currentIndex = 0
+
+                  const playNextAudio = () => {
+                    if (currentIndex < instructionAudios.length) {
+                      const audio = new Audio(instructionAudios[currentIndex])
+                      audio.onended = () => {
+                        currentIndex++
+                        playNextAudio()
+                      }
+                      audio.play()
+                    }
+                  }
+
+                  playNextAudio()
+                }}
+                className="text-gray-500 hover:text-black transition-colors"
+              >
+                <AudioIcon width={32} height={32} />
+              </button>
             </div>
             <h3 className="text-lg md:text-xl font-semibold mb-4 text-gray-700 leading-snug">
               Here are the instructions
@@ -60,10 +96,16 @@ function PageContent() {
               <li>Read or listen to each question carefully.</li>
               <li>Read or listen to the options one by one.</li>
               <li>Take your time for each question, no need to rush.</li>
-              <li>Tap on i to listen.</li>
+              <li>
+                Tap on{' '}
+                <span className="inline-block align-middle">
+                  <AudioIcon width={20} height={20} />
+                </span>{' '}
+                to listen.
+              </li>
               <li>
                 Answer is only saved when you tap –
-                <span className="ml-2 px-2 py-1 rounded-md bg-blue-200 text-blue-700 text-sm font-medium">
+                <span className="ml-2 px-2 py-1 rounded-full bg-primary-700 text-white text-sm font-medium">
                   Next
                 </span>
               </li>
